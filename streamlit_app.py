487d60433aab957bad531e441ff61bbcde498fc7
omarcorrales1070 <ocorrales@cibernetica.net>
2026-07-24 10:24:02 -0500

    Comment out dataframe display and stop
    Comment out dataframe display and stop execution.

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,8 +28,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # convert the snowpark dataframe to pandas dataframe para usar la funcion LOC
 
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop
+#st.dataframe(pd_df)
+#st.stop
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -43,6 +43,11 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st_write ('The search value for ', fruit_chosen, ' is ', search_on, '.')
+
+      
         st.subheader(fruit_chosen + 'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ fruit_chosen)  
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True) 
