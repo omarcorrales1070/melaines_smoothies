ca27233cf16a17b28163a30b9d2bafb3f237e7f0
omarcorrales1070 <ocorrales@cibernetica.net>
2026-07-24 09:42:23 -0500

    Fetch and display watermelon data from API
    Added API call to fetch watermelon data and display it in a dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-
+import requests 
 
 cnx=st.connection("snowflake")
 session= cnx.session()
@@ -36,6 +36,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True) 
 
     st.write(ingredients_string)
 
@@ -45,11 +47,6 @@ if ingredients_list:
     #st.write(my_insert_stmt)
 
     time_to_insert = st.button('Submit Order')
-
-    import requests  
-    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-    #st.text(smoothiefroot_response.json())
-    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True) 
   
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
