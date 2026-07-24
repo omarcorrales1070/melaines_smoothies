27d407424a9678132579f83364f4ff5dc5700314
omarcorrales1070 <ocorrales@cibernetica.net>
2026-07-24 10:11:48 -0500

    Update dataframe handling in streamlit_app.py
    Comment out the original dataframe display and stop calls, and add conversion to pandas dataframe for display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,14 @@ st.write ('The name on your smoothie will be:', name_on_order)
 
 #session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+# convert the snowpark dataframe to pandas dataframe para usar la funcion LOC
+
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
